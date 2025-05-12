1bbab0dfd94414582f848dbebbcaf37e6f468669
Sandro Magalhães <sandro.a.magalhaes@inesctec.pt>
2025-05-12 15:09:01 +0100

    Refactor git.php to add missing commands for enabling PHP extensions and restarting Apache

--- git.php
+++ git.php
@@ -23,11 +23,21 @@ echo "done";
 $output = shell_exec('git clean -f -d 2>&1');
 echo "<pre>$output</pre>";
 echo "done";
- 
+
 // Executar o comando 'git pull'
 $output = shell_exec('git pull 2>&1');
  
 // Exibir a saída do comando
+echo "<pre>$output</pre>";
+echo "done";
+
+
+
+$output = shell_exec('phpenmod password 2>&1');
+echo "<pre>$output</pre>";
+echo "done";
+
+$output = shell_exec('systemctl restart apache2 2>&1');
 echo "<pre>$output</pre>";
 echo "done";
  
