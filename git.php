55c64fb8d8e8a5c8b00c526c5092a18be54552bd
Sandro Magalhães <sandro.a.magalhaes@inesctec.pt>
2025-05-14 08:04:04 +0100

    Enhance curl command in git.php with additional headers for improved request handling

--- git.php
+++ git.php
@@ -32,23 +32,23 @@ echo "<pre>$output</pre>";
 echo "done";
 
 
-// $output = shell_exec('curl \'https://my.microsoftpersonalcontent.com/personal/2c99c02e615859bb/_layouts/15/download.aspx?UniqueId=85089b56-9be9-4acc-bd61-f05ef5738aa3&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiIyYTkzMjA2ZC02YjQwLTQ1MzAtYTA0YS01M2RlNjI3YjkyMzYiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzQ3MTUwMzY0In0.otfi-xBeIbkb5juR239Y4luga-4W1dPZzcDX5C5OnXhWll97NzN-ovAT8_muz-Magnaml5o5bfCHgHUNjvg_s2ipZ2LklVXR87txAzocuxCzAFqhZ9jP9GFrWrsftFUojFQ9bOopKAIOZxcTK2kLZrcp3GjKOjNwToQ-P8SfFHOHv2QRZdo0s4h-O1w1pwtOraY_nIaTf5GtzNSm6zJjO7OkDSeUuMrWEmTRrcom_S3q8P-3Dfscxzp7ylRA25lhaAPB5P4b9GVliIEALD6WIoLrBnSljFuMpmHa21O1cbzM5gETimzGUWNAcnIyQRzbnIfRmcizvselR7kk3gsTiNelBH5UqWO4Xlp4qeFmtILEn2lZLCRIWbjXS6VXZBvTip9INi5Ma2LRmkzjjBUhdAArT9OFW1-b_8486bc1hiCYDv4ZiLLFWegWP3GXptG19u-5w6fXkGfmpqw_eM3g2bY-E5esraJdKiaqkKo4Hc6KIZ05wHxWEimzDuimaNgi95VOiQ66YFnB-lUcdDTHSQ.kNHadxspH_fjqYGF8kLKFQASpvO2MiUn8y_299HzzCI&ApiVersion=2.0\' \
-//   -H \'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\' \
-//   -H \'accept-language: pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7\' \
-//   -H \'priority: u=0, i\' \
-//   -H \'referer: https://onedrive.live.com/\' \
-//   -H \'sec-ch-ua: "Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"\' \
-//   -H \'sec-ch-ua-mobile: ?0\' \
-//   -H \'sec-ch-ua-platform: "Linux"\' \
-//   -H \'sec-fetch-dest: iframe\' \
-//   -H \'sec-fetch-mode: navigate\' \
-//   -H \'sec-fetch-site: cross-site\' \
-//   -H \'sec-fetch-storage-access: active\' \
-//   -H \'sec-fetch-user: ?1\' \
-//   -H \'upgrade-insecure-requests: 1\' \
-//   -H \'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36\' --output omicbots.sql 2>&1');
-// echo "<pre>$output</pre>";
-// echo "done";
+$output = shell_exec('curl \'https://my.microsoftpersonalcontent.com/personal/2c99c02e615859bb/_layouts/15/download.aspx?UniqueId=85089b56-9be9-4acc-bd61-f05ef5738aa3&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiIyYTkzMjA2ZC02YjQwLTQ1MzAtYTA0YS01M2RlNjI3YjkyMzYiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzQ3MjA5NjExIn0.tMHxu9kFki0Y41zNXb0hCEfx3B5LYFOmEAHGtV_LqB8cZru8D49E7Jt1EuK1UI1zI_0F1zs_X-rGYplPjkLd-u0dA-ykJjy8nRbt3-rgb80h750vO2NXiRwKJ-0Q0tRxQesyz7vVaRmGfQWY4r3Z6LIoL6ZsYvzYdyJrjANOs9s-wLXvQCdiE4Ttq4jZF7ka5NYUwdpbzMu6w8QXEPmKmaNtoyFVnDLUR0pdUrhE5jiEr8_l1pVtrGovMe2DA6SUs72zsxPEt6XFFiRXN-YZwFBzbsS4w0FZvraIv0I_FbGAojkrEq2aUatvuhsNx7L8192Llw2smdN0hjYMrlSXwbh7b_MjVmL-Z-AusyYLK0J4UJ-fwaROESDlHho6WJGd4f7MULxt1DOYkNllsa4_FaFfMtvN2VOVnlKM_Mz8W8lJg9XSoMsWHoIotsABgBO05LCmIhcEg3EGIcpVn-j5EJ3hHX9LOw2-G0yeP1fgJdf1wKhiEgmvuqy8U1XzLjSERzGf3iThTa32PgRopOM7kw.Y7YXhiYPfsDmrTuBHXz91D0L-12PLWoR5b4X_lq1ccs&ApiVersion=2.0\' \
+  -H \'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\' \
+  -H \'accept-language: pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7\' \
+  -H \'priority: u=0, i\' \
+  -H \'referer: https://onedrive.live.com/\' \
+  -H \'sec-ch-ua: "Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"\' \
+  -H \'sec-ch-ua-mobile: ?0\' \
+  -H \'sec-ch-ua-platform: "Linux"\' \
+  -H \'sec-fetch-dest: iframe\' \
+  -H \'sec-fetch-mode: navigate\' \
+  -H \'sec-fetch-site: cross-site\' \
+  -H \'sec-fetch-storage-access: active\' \
+  -H \'sec-fetch-user: ?1\' \
+  -H \'upgrade-insecure-requests: 1\' \
+  -H \'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36\' --output /var/www/html/omicbots/omicbots-db/omicbots.sql 2>&1');
+echo "<pre>$output</pre>";
+echo "done";
 
 $output = shell_exec('pwd 2>&1');
 echo "<pre>$output</pre>";
