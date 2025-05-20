53bbea7a79c7e0a16a07de40a784555d003ce168
Sandro Magalhães <sandro.a.magalhaes@inesctec.pt>
2025-05-20 23:02:37 +0100

    Update download-db.php to modify curl command for downloading SQL file with updated headers and parameters

--- download-db.php
+++ download-db.php
@@ -13,21 +13,24 @@ echo "<pre>$output</pre>";
 $output = shell_exec("git config --global --add safe.directory /var/www/html/omicbots/omicbots-db 2>&1");
 echo "<pre>$output</pre>";
 
-$output = shell_exec('curl \'https://my.microsoftpersonalcontent.com/personal/2c99c02e615859bb/_layouts/15/download.aspx?UniqueId=85089b56-9be9-4acc-bd61-f05ef5738aa3&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiIyYTkzMjA2ZC02YjQwLTQ1MzAtYTA0YS01M2RlNjI3YjkyMzYiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzQ3MjA5NjExIn0.tMHxu9kFki0Y41zNXb0hCEfx3B5LYFOmEAHGtV_LqB8cZru8D49E7Jt1EuK1UI1zI_0F1zs_X-rGYplPjkLd-u0dA-ykJjy8nRbt3-rgb80h750vO2NXiRwKJ-0Q0tRxQesyz7vVaRmGfQWY4r3Z6LIoL6ZsYvzYdyJrjANOs9s-wLXvQCdiE4Ttq4jZF7ka5NYUwdpbzMu6w8QXEPmKmaNtoyFVnDLUR0pdUrhE5jiEr8_l1pVtrGovMe2DA6SUs72zsxPEt6XFFiRXN-YZwFBzbsS4w0FZvraIv0I_FbGAojkrEq2aUatvuhsNx7L8192Llw2smdN0hjYMrlSXwbh7b_MjVmL-Z-AusyYLK0J4UJ-fwaROESDlHho6WJGd4f7MULxt1DOYkNllsa4_FaFfMtvN2VOVnlKM_Mz8W8lJg9XSoMsWHoIotsABgBO05LCmIhcEg3EGIcpVn-j5EJ3hHX9LOw2-G0yeP1fgJdf1wKhiEgmvuqy8U1XzLjSERzGf3iThTa32PgRopOM7kw.Y7YXhiYPfsDmrTuBHXz91D0L-12PLWoR5b4X_lq1ccs&ApiVersion=2.0\' \
+$output = shell_exec('curl \'https://inesctecpt-my.sharepoint.com/personal/renan_tosin_office365_inesctec_pt/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2Frenan%5Ftosin%5Foffice365%5Finesctec%5Fpt%2FDocuments%2FFicheiros%20de%20Conversa%20do%20Microsoft%20Teams%2Fomicbots%5F2025%5F05%5F20%2Esql\' \
   -H \'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\' \
   -H \'accept-language: pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7\' \
+  -b \'MSFPC=GUID=de0adfcea7f94580999904c4b93c4a54&HASH=de0a&LV=202502&V=4&LU=1739440069970; ExcelWacDataCenter=PNL1; ExcelWacDataCenterSetTime=2025-05-09T07:35:19.803Z; FeatureOverrides_experiments=[]; PowerPointWacDataCenter=GEU9; WacDataCenter=GEU9; msal.cache.encryption=%7B%22id%22%3A%220196c994-f9b8-792d-b1f2-cef5a1bed283%22%2C%22key%22%3A%22uhAJH4uUP1pyFC4S3Ie8cYp3T-Qm6zNFuX9QcIJzWwE%22%7D; WSS_FullScreenMode=false; PowerPointWacDataCenterSetTime=2025-05-13T12:20:15.074Z; WacDataCenterSetTime=2025-05-13T12:20:15.074Z; MicrosoftApplicationsTelemetryDeviceId=11302cc2-690b-4ae0-85d4-6d4a3ce1c50f; FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+VjE0LDBoLmZ8bWVtYmVyc2hpcHx1cm4lM2FzcG8lM2F0ZW5hbnRhbm9uIzExOWU1ZDM3LTQzYTgtNGY4Ny1iNTJiLWVlZDI4ODk5OWJmZiwwIy5mfG1lbWJlcnNoaXB8dXJuJTNhc3BvJTNhdGVuYW50YW5vbiMxMTllNWQzNy00M2E4LTRmODctYjUyYi1lZWQyODg5OTliZmYsMTMzOTIyMjk2OTgwMDAwMDAwLDAsMTMzOTIzMzQ4MjkyNzk4MDQ0LDAuMC4wLjAsMjU4LDExOWU1ZDM3LTQzYTgtNGY4Ny1iNTJiLWVlZDI4ODk5OWJmZiwsLDI5OTZhMjEwLTA3MzgtNGQ4Mi04ZDIxLTNhYTZkOTVlMmZjZCxjNDFhYTBhMS01MGEwLWMwMDAtZGJlMy1kMzNlODMzYmY1ZDIsaGZYaTRtY2dUa3F0WTFENitGT0ZPZywwLDAsMCwsLCwyNjUwNDY3NzQzOTk5OTk5OTk5LDAsLCwsLCwsMCwsMTg5MDQxLGQ3V19lbmZqWjMyVl9SdXI5aVJ3T1A1RThjOCw3bnhiczlWbEp0dlRXYUo4aFVRRkxCNndRSXMsdmFMalB4b3p5RXRqS0RuTU9mcmFYTVJSc216c3lFV09nQUZDbURpSW1GYjk5eGR1QlhQMUg2UnFwTFdKUUVndjNsaXFqbGFtMjNQTkRnS2lqRENNUDBidmlxcUgyVGs2QnVGRFAzeHdNdTVWajhRNm5ObkJpWEZMVlRqR3Btc1V5VkdtOGtyM1N3bVZSMkRRVjNPSmgwbkZhcVF2WG9nRTloRVB2Sm1zWUhjVzFOVVUzZFN1bFk2M1pyQ1lCdDZrVHVpSjFTb011dkpCZWpaMzgrZnJCUWtnREhnaVJINTN5elphbm5LQTBnQStib0JtaUl5OWdIc0xyK0dZZTlxN2F0OWhZbVNzOWJtVndIeGZrRExIYW5GZGRwUm9yMy9GSGpLTHNjM2UzT0k5VjF5V3BGcmh0ZDVXU0d0dVd1R2NhTGNZMk5xQjJLMWNLekJTOEE1b1VRPT08L1NQPg==; ai_session=XGgn3lVFuDDqvUD8lwgGFz|1747778425503|1747778425508\' \
+  -H \'if-range: "{FFE28F7F-D978-4287-8B16-16FC774D535A},3"\' \
   -H \'priority: u=0, i\' \
-  -H \'referer: https://onedrive.live.com/\' \
-  -H \'sec-ch-ua: "Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"\' \
+  -H \'range: bytes=137980224-137980224\' \
+  -H \'referer: https://inesctecpt-my.sharepoint.com/personal/renan_tosin_office365_inesctec_pt/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Frenan%5Ftosin%5Foffice365%5Finesctec%5Fpt%2FDocuments%2FFicheiros%20de%20Conversa%20do%20Microsoft%20Teams%2Fomicbots%5F2025%5F05%5F20%2Esql&parent=%2Fpersonal%2Frenan%5Ftosin%5Foffice365%5Finesctec%5Fpt%2FDocuments%2FFicheiros%20de%20Conversa%20do%20Microsoft%20Teams&ga=1\' \
+  -H \'sec-ch-ua: "Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"\' \
   -H \'sec-ch-ua-mobile: ?0\' \
   -H \'sec-ch-ua-platform: "Linux"\' \
   -H \'sec-fetch-dest: iframe\' \
   -H \'sec-fetch-mode: navigate\' \
-  -H \'sec-fetch-site: cross-site\' \
-  -H \'sec-fetch-storage-access: active\' \
+  -H \'sec-fetch-site: same-origin\' \
   -H \'sec-fetch-user: ?1\' \
+  -H \'service-worker-navigation-preload: {"supportsFeatures":[1855,61313]}\' \
   -H \'upgrade-insecure-requests: 1\' \
-  -H \'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36\' --output /var/www/html/omicbots/omicbots-db/omicbots.sql 2>&1');
+  -H \'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36\' --output omicbots.sql 2>&1');
 echo "<pre>$output</pre>";
 echo "done";
 
